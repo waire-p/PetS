d43ad675c429954db0d9e6454fc35e3c16132550
waire-p <arzhaevaalisa@gmail.com>
2025-04-22 18:12:03 +0300

    Добавление таблицы объявлений питомцев

---
+++ data/__all_models.py
@@ -0,0 +1,1 @@
+from . import animal_cards
---
+++ data/animal_cards.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+import datetime
+
+class PetCard(SqlAlchemyBase):
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(""), nullable=False, unique=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    age =  sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    gender = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    vaccinations = sqlalchemy.Column(sqlalchemy.String)
+    diseases = sqlalchemy.Column(sqlalchemy.String)
+    city = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=False, default=datetime.datetime.now )
