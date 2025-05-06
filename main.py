0dd95847b3dffef94f2e10dc6b0353c1a51c84e0
Anton <toscha.aksenov@gmail.com>
2025-05-06 23:10:57 +0300

    added filter without bindings

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.config['SECRET_KEY'] = '0yKJg9B62haFjq7K2gh1'
 
 @app.route('/')
 def main_page():
-    return render_template('base.html')
+    return render_template('base.html') and render_template('main.html')
 
 
 @app.route('/pets')
@@ -19,6 +19,30 @@ def pet_catalog():
 
     #posts = sqlalchemy.paginate(query, page=1, per_page=20, error_out=False).items
     return render_template('pet_catalog.html')
+
+
+@app.route('/about')
+def about():
+    with open('data/about.txt', encoding='utf-8') as file:
+        about_text = file.read()
+    return render_template('about.html', about_text=about_text)
+
+
+@app.route('/articles')
+def articles():
+    articles = [
+        {
+            "title": "Как подготовить дом к появлению питомца",
+            "summary": "Полезные советы перед тем, как взять щенка.",
+            "url": "https://ru.wikihow.com/%D0%BF%D0%BE%D0%B4%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D0%B8%D1%82%D1%8C%D1%81%D1%8F-%D0%BA-%D0%BF%D0%BE%D1%8F%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D1%8E-%D1%89%D0%B5%D0%BD%D0%BA%D0%B0-%D1%83-%D0%B2%D0%B0%D1%81-%D0%B4%D0%BE%D0%BC%D0%B0"
+        },
+        {
+            "title": "Почему важно забирать животных из приюта",
+            "summary": "Аргументы в пользу усыновления, а не покупки.",
+            "url": "https://harpersbazaar.kz/10-prichin-zabrat-zhivotnoe-iz-prijuta/"
+        }
+    ]
+    return render_template("articles.html", articles=articles)
 
 
 @app.route('/pets/<card_id>')
